8b21d8798053cee61d515eef280b71cdde82702c
amarjeetsahoo <amarjeethappysahoo@gmail.com>
2021-01-06 13:26:42 +0530

    Back-end Deployed

--- client/api/index.js
+++ client/api/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const url = 'http://localhost:5000/posts';
+//const url = 'http://localhost:5000/posts';
+const url = 'https://memories-ahs2000.herokuapp.com';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
